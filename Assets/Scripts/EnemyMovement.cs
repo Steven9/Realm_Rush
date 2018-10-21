937c3d52033f55816b421b14f94fc768e9321b90
Yuchu Ma <yuchu.ma@berkeley.edu>
2018-10-21 14:49:29 -0700

    Realm_Rush prototype

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour 
+{
+    [SerializeField] float movementPeriod = .5f;
+    [SerializeField] ParticleSystem goalParticle;
+
+	// Use this for initialization
+	void Start () 
+    {
+        PathFinder pathFinder = FindObjectOfType<PathFinder>();
+        var path = pathFinder.GetPath();
+        StartCoroutine(FollowPath(path));
+    }
+
+    IEnumerator FollowPath(List<Waypoint> path)
+    {
+        foreach (Waypoint waypoint in path) 
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(movementPeriod);
+        }
+        SelfDestruct();
+    }
+
+    private void SelfDestruct()
+    {
+        var vfx = Instantiate(goalParticle, transform.position, Quaternion.identity);
+        vfx.Play();
+        Destroy(vfx.gameObject, vfx.main.duration);
+
+        Destroy(gameObject);
+    }
+}
